7348274243f5c7ee1938850c37930fb2a6c1f5b8
SlinkierSwine87 <khanipovm@gmail.com>
2021-04-25 23:01:02 +0500

    Добавил картинку

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 class Config:
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = 'yandex_lyceumproject'
     UPLOAD_PATH = 'static/storage'  # Папка для загрузки файлов
     VIDEOS_PER_PAGE = 10  # Кол-во видео на одной странице
